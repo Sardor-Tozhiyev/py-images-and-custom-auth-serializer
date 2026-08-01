900959e0182dff43b4a267403a79f5cc676824d4
Sardor <sardor.tozhiev1994@gmail.com>
2026-08-01 19:33:28 +0300

    fixed

--- cinema/models.py
+++ cinema/models.py
@@ -51,7 +51,11 @@ class Movie(models.Model):
     duration = models.IntegerField()
     genres = models.ManyToManyField(Genre)
     actors = models.ManyToManyField(Actor)
-    image = models.ImageField(upload_to=movie_image_path, null=True, blank=True)
+    image = models.ImageField(
+        upload_to=movie_image_path,
+        null=True,
+        blank=True
+    )
 
     class Meta:
         ordering = ["title"]
